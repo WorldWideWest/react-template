ad4b009c2c7cfbd56fe0066312be063ece47082a
WorldWideWest <dzeno.dzafic@windowslive.com>
2024-03-07 18:42:19 +0100

    Redirect fix

--- src/pages/sigonutRedirect/index.tsx
+++ src/pages/sigonutRedirect/index.tsx
@@ -1,14 +1,10 @@
 import { useNavigate } from 'react-router-dom';
-import { userManager } from '../../oidc';
 import { useEffect } from 'react';
 
 function SignOutRedirect() {
   const navigate = useNavigate()
     useEffect(() => {
-        userManager.signoutRedirectCallback().then(data => {
-            console.log(data);
             navigate('/')
-        });
     })
   return (
     <div className="App">
