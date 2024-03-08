76ceb4e4430410d28dafae35e94dd6bdd90ad3f2
WorldWideWest <dzeno.dzafic@windowslive.com>
2024-03-08 10:53:55 +0100

    Restructuring project

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import Home from './pages/home';
-import SigninRedirect from './pages/signinRedirect';
-import Welcome from './pages/welcome';
-import SignOutRedirect from './pages/sigonutRedirect';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './components/pages/homePage'
+import SigninRedirect from './components/pages/signinRedirect'
+import Welcome from './components/pages/welcome'
+import SignOutRedirect from './components/pages/sigonutRedirect'
 export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' Component={Home}/>
-        <Route path='/signin-redirect' Component={SigninRedirect}/>
-        <Route path='/welcome' Component={Welcome}/>
-        <Route path='/signout-redirect' Component={SignOutRedirect}/>
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" Component={Home} />
+                <Route path="/signin-redirect" Component={SigninRedirect} />
+                <Route path="/welcome" Component={Welcome} />
+                <Route path="/signout-redirect" Component={SignOutRedirect} />
+            </Routes>
+        </BrowserRouter>
+    )
 }
-
